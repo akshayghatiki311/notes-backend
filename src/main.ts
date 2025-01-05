f36ce55cab780c284375518332f9bba8bfc8e5f5
Akshay Ghatiki <akshayghatiki@Akshays-MacBook-Pro.local>
2025-01-05 22:57:35 +0530

    dotenv support

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: process.env.ENABLE_CORS, // Vite default port
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
