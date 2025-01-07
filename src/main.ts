5a6ff0179ec023a2a202103320402d508bb5877f
Akshay Ghatiki <akshayghatiki@Akshays-MacBook-Pro.local>
2025-01-07 22:26:10 +0530

    allowed multiple origins

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [process.env.CORS_ORIGIN_1, process.env.CORS_ORIGIN_2, process.env.CORS_ORIGIN_2];
+
   app.enableCors({
-    origin: process.env.ENABLE_CORS, // Vite default port
+    origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
